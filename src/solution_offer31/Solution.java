26e7e3f8f84c3f93be600f43be57d7eab6efebed
Terminator-hui <103932641@qq.com>
2020-12-11 22:03:50 +0800

    LeetCode_Solutions

--- src/solution_offer31/Solution.java
+++ src/solution_offer31/Solution.java
@@ -3,17 +3,34 @@ package solution_offer31;
 import java.util.Stack;
 
 class Solution {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
-        if(pushed.length==0||pushed==null)return true;
-        int index = 0;
+    public static boolean validateStackSequences(int[] pushed, int[] popped)
+//    {
+//        if(pushed.length==0||pushed==null)return true;
+//        int index = 0;
+//        Stack<Integer> stack = new Stack<>();
+//        for (int i = 0; i < pushed.length; i++) {
+//            stack.push(pushed[i]);
+//            while (!stack.isEmpty()&&stack.peek() == popped[index]){
+//                stack.pop();
+//                index ++;
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+    {
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < pushed.length; i++) {
-            stack.push(pushed[i]);
-            while (!stack.isEmpty()&&stack.peek() == popped[index]){
+        int x = 0;
+        for (int num : pushed) {
+            stack.push(num);
+            while (!stack.isEmpty()&&stack.peek()==popped[x]){
                 stack.pop();
-                index ++;
+                x++;
             }
         }
         return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validateStackSequences(new int[]{1, 2, 3, 4, 5}, new int[]{4, 5, 3, 2, 1}));
     }
 }
--- src/solution_offer32/Solution.java
+++ src/solution_offer32/Solution.java
@@ -1,10 +1,7 @@
 
 package solution_offer32;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Definition for a binary tree node.
@@ -26,25 +23,43 @@ class Solution {
         }
     }
 
-    List<List<Integer>> res=new ArrayList<>();
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        helper(root,0);
+    //    List<List<Integer>> res=new ArrayList<>();
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//        helper(root,0);
+//        return res;
+//    }
+//
+//    private void helper(TreeNode root,int level){
+//        if(root==null)
+//            return;
+//        if(res.size()==level){
+//            res.add(new ArrayList<>());
+//        }
+//        if(level%2==0){
+//            res.get(level).add(root.val);
+//
+//        }else{
+//            res.get(level).add(0,root.val);
+//        }
+//        helper(root.left,level+1);
+//        helper(root.right,level+1);
+//    }
+    public int[] levelOrder(TreeNode root) {
+        if(root == null) return new int[0];
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty())
+        {
+            TreeNode node  = queue.poll();
+            if (node.left!=null) queue.add(node.left);
+            if(node.right!=null) queue.add(node.right);
+            ans.add(node.val);
+        }
+        int[] res =new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
         return res;
-    }
-
-    private void helper(TreeNode root,int level){
-        if(root==null)
-            return;
-        if(res.size()==level){
-            res.add(new ArrayList<>());
-        }
-        if(level%2==0){
-            res.get(level).add(root.val);
-
-        }else{
-            res.get(level).add(0,root.val);
-        }
-        helper(root.left,level+1);
-        helper(root.right,level+1);
     }
 }
